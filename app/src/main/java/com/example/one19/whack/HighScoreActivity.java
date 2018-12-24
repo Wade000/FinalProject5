555744aaba7341f1f9c79545c26c4bb610f1c1cc
turtle0328 <one19980328@gmail.com>
2018-12-25 01:56:37 +0800

    HighScore edit

--- app/src/main/java/com/example/one19/whack/HighScoreActivity.java
+++ app/src/main/java/com/example/one19/whack/HighScoreActivity.java
@@ -1,9 +1,18 @@
 package com.example.one19.whack;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-
-
+import java.util.ArrayList;
 
 public class HighScoreActivity extends Activity {
 
@@ -13,6 +22,44 @@ public class HighScoreActivity extends Activity {
 
 
 
+
+
+
+
+
+
+/*
+        listView = findViewById(R.id.listView);
+        adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+        dbrw = new MyDBHelper(this)
+                .getWritableDatabase();
+
+
+        getdata=getIntent().getExtras().getInt("key");
+        count=getIntent().getExtras().getInt("count");
+        getstrcount=String.valueOf(count);
+
+        dbrw.execSQL("INSERT INTO myTable(book, price) VALUES(?,?)", new Object[]{"玩家"+count,
+                getdata});
+
+        Cursor c;
+        c = dbrw.rawQuery("SELECT * FROM myTable",null);
+        c.moveToFirst();
+        items.clear();
+        Toast.makeText(HighScoreActivity.this,"共有" + c.getCount() +
+                "筆資料",Toast.LENGTH_SHORT).show();
+        for(int i = 0; i < c.getCount(); i++){
+            items.add("\t\t\t\t"+c.getString(0) +
+                    "\t\t\t\t\t\t\t\t 分數:" +"\t\t\t\t"+ c.getString(1)+"\t\t\t\t分");
+            c.moveToNext();
+        }
+        adapter.notifyDataSetChanged();
+        c.close();
+*/
+
+
+        // TODO read in file to ListView
 
 
 
--- app/src/main/java/com/example/one19/whack/SQLiteOpenHelper.java
+++ app/src/main/java/com/example/one19/whack/SQLiteOpenHelper.java
@@ -1,10 +1,31 @@
 package com.example.one19.whack;
 
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 
 class MyDBHelper extends SQLiteOpenHelper {
+    private static final String name = "mdatabase.db";
+    private static final int version = 2;
 
+    MyDBHelper(Context context) {
+        super(context, name, null, version);
+    }
 
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE myTable(book integer NO NULL,price integer NO NULL)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion,
+                          int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS myTable");
+        onCreate(db);
+    }
 }
+
+
 
